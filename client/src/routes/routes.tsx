770c8557b53445b684829d694d0d28b15b2648c7
tpvan5521 <tpvan5521@gmail.com>
2022-03-21 20:24:47 +0700

    update routes

--- client/src/routes/routes.tsx
+++ client/src/routes/routes.tsx
@@ -5,15 +5,9 @@ import { AuthProvider, AuthContext } from "context/auth/AuthContext";
 import PageLayout from "components/Layout";
 import PageLoader from "components/Loader/PageLoader";
 const HomePage = React.lazy(() => import("pages/Home"));
-<<<<<<< HEAD
-const PostPage = React.lazy(() => import("pages/Post"));
-const ProfilePage = React.lazy(() => import("pages/Profile"));
-const LoginPage = React.lazy(() => import("pages/Login"));
-=======
 const PostPage = React.lazy(() => import("pages/Post/WritePost"));
 const ProfilePage = React.lazy(() => import("pages/Profile"));
 const LoginPage = React.lazy(() => import("pages/Auth/LogIn"));
->>>>>>> 901428e (update homepage & config)
 
 export default function App() {
   return (
